d8be0bb0a20ab3ab01a6113258f3bdffc891a6bf
Axel Ander Chacón Tasaico <85888364+Axel0203@users.noreply.github.com>
2022-05-13 17:41:37 -0500

    added a link to the repo in page "Nosotros"

--- nosotros.php
+++ nosotros.php
@@ -6,7 +6,7 @@
     <hr class="my-2">
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum hic eveniet recusandae esse. Totam fugit ipsa tenetur dolor quas, repellendus maxime eveniet. Inventore fugit rerum facilis ipsa earum quos ad! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis eum minima iure libero, magnam ipsam! Quis, hic eveniet quia doloremque consequuntur deserunt in blanditiis ex, labore eius qui enim tempora.</p>
     <p class="lead">
-        <a class="btn btn-primary btn-lg" href="#" role="button">Click me</a>
+        <a class="btn btn-primary btn-lg" href="https://github.com/Axel0203/lab8-dai" role="button">Click me</a>
     </p>
 </div>
 
